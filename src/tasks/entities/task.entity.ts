e1e67b61fbb0371d3b00c6c7f20f6f77210270ae
Wesley Medeiros <wmedeirosc3@gmail.com>
2024-08-16 13:32:50 -0300

    feat: users and tasks

---
+++ src/tasks/entities/task.entity.ts
@@ -0,0 +1,10 @@
+export class Task {
+  taskId: string;
+  title: string;
+  description: string;
+  createdAt: string;
+  dueDate: string;
+  completed_at: string;
+  status: string;
+  priority: string;
+}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,7 @@
+export class User {
+  id: string;
+  name: string;
+  mail: string;
+  status: string;
+  profilePicture: string;
+}
